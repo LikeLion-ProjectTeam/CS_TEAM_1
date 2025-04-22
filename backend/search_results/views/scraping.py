194da69e803b16cdc1b4b6a1dfd3c9c8301e7c90
Pixel0613 <68191099+Pixel0613@users.noreply.github.com>
2025-04-21 19:54:05 -0500

    Update scraping.py

--- backend/search_results/views/scraping.py
+++ backend/search_results/views/scraping.py
@@ -43,7 +43,7 @@ def search_google(request):
             # print("SerpAPI 응답 keys:", results.keys())
             
             # 검색 결과 중 상위 1개만 추출 (for testing)
-            top_results = results.get("organic_results", [])[:1]
+            top_results = results.get("organic_results", [])[:5]
 
             # 검색 결과 없을 경우, return
             if not top_results:
